c286fedfd78c7123eab0cb2ed6937bc515923dd3
Chase Goldfeld <goldfeld.chase@gmail.com>
2020-01-15 11:41:53 -0500

    Update utility.py

--- lambdata_AuFeld/utility.py
+++ lambdata_AuFeld/utility.py
@@ -3,14 +3,15 @@ Define a class named slope-intercept which can be constructed by the slope (m) a
 """
 
 class Slope_Intercept:
-    def __init__ (self, m, b):
+    def __init__ (self, m, x, b):
         self.slope = m
         self.intercept = b
+        self.x_intercept = x
     def a straight line (self):
         """
         be a able to plot a line with the slope-intercept equation.
         """
-        return self.slope * (x) + self.intercept
+        return self.slope * self.x_intercept + self.intercept
 
-ySlope_Intercept = Slope_Intercept(1,-5)
+ySlope_Intercept = Slope_Intercept(1, 2, -5)
 print(ySlope_Intercept.line())
